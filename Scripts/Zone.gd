6a5e30d25d0672cb9aa272ab3c6ea514be5e0c9b
Chondrite_ <61230998+CH0NDRITE@users.noreply.github.com>
2026-02-28 22:37:25 +0000

    minor tweaks

--- Scripts/Zone.gd
+++ Scripts/Zone.gd
@@ -1,26 +1,30 @@
 extends Area2D
 
 var combo = 0
-var current_flower = null
+var currentPlant = null
+var currentHealth = 6;
 
 func _on_area_entered(area):
 	if area.has_method("score"):
-		current_flower = area
+		currentPlant = area
 
 func _on_area_exited(area):
-	if area == current_flower:
+	if area == currentPlant:
 		if not area.was_pressed() && area.checkType() == false:
 			combo = 0
 			print(combo)
-		current_flower = null
+		currentPlant = null
 
 func _process(_delta):
-	if current_flower and Input.is_action_just_pressed("space"):
-		var result = current_flower.score()
+	if currentPlant and Input.is_action_just_pressed("space"):
+		var result = currentPlant.score()
 
 		if result == 1:
+			currentHealth -= 1
 			combo += 1
 		else:
+			currentHealth -= 2
 			combo = 0
-
-		print(combo)
+		
+		print("health = ", currentHealth)
+		print("combo = ", combo)
